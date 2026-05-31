1986d51ff784a16fd2a3a45b9fd9442a0ad09536
TNKDieguexPCLinux <diegoaguilar20040303@gmail.com>
2026-05-31 14:38:56 -0400

    ajout de ProduitException et de la méthode trouverProduitsParFamille dans ProduitRepository

---
+++ src/main/java/com/dieguex/inventaireMaisonBackend/Exceptions/ProduitException.java
@@ -0,0 +1,7 @@
+package com.dieguex.inventaireMaisonBackend.Exceptions;
+
+public class ProduitException extends Exception {
+    public ProduitException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/dieguex/inventaireMaisonBackend/persistence/ProduitRepository.java
+++ src/main/java/com/dieguex/inventaireMaisonBackend/persistence/ProduitRepository.java
@@ -2,6 +2,19 @@ package com.dieguex.inventaireMaisonBackend.persistence;
 
 import com.dieguex.inventaireMaisonBackend.model.Produit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface ProduitRepository extends JpaRepository<Produit, Long> {
+    @Query(
+        """
+            SELECT p FROM Produit p
+            JOIN p.famille f
+            WHERE f.uuid = :familleUuid
+        """)
+    Optional<List<Produit>> trouverProduitsParFamille(@Param("familleUuid") UUID familleUuid);
 }

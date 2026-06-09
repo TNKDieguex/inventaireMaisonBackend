6c34acea50741aa0225954d1b059d8860adaca5c
TNKDieguexPCLinux <diegoaguilar20040303@gmail.com>
2026-06-09 12:47:29 -0400

    ajout des endpoints de modification, de suppression et de récupération des produits, et de génération de la liste d'alertes et d'achats dans ProduitController

--- src/main/java/com/dieguex/inventaireMaisonBackend/controller/ProduitController.java
+++ src/main/java/com/dieguex/inventaireMaisonBackend/controller/ProduitController.java
@@ -1,20 +1,18 @@
 package com.dieguex.inventaireMaisonBackend.controller;
 
-import com.dieguex.inventaireMaisonBackend.dto.CreationProduitsDto;
-import com.dieguex.inventaireMaisonBackend.dto.ProduitDto;
+import com.dieguex.inventaireMaisonBackend.dto.*;
 import com.dieguex.inventaireMaisonBackend.exceptions.FamilleException;
+import com.dieguex.inventaireMaisonBackend.exceptions.ProduitException;
 import com.dieguex.inventaireMaisonBackend.service.ProduitService;
 import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/produits")
@@ -34,5 +32,48 @@ public class ProduitController {
         return ResponseEntity.status(HttpStatus.CREATED).body(produitDtoList);
     }
 
+    @PatchMapping("/{uuid}/quantite")
+    public ResponseEntity<ProduitDto> modifierQuantiteProduit(@PathVariable UUID uuid,
+                                                              @Valid @RequestBody UpdateQuantiteProduitDto updateQuantiteProduitDto) throws ProduitException {
+        logger.info("Modification de la quantité du produit : {}", uuid);
+        ProduitDto produitDto = produitService.modifierQuantiteProduit(uuid, updateQuantiteProduitDto.quantite())
+                .orElseThrow();
+        return ResponseEntity.ok().body(produitDto);
+    }
 
+    @PatchMapping("/{uuid}/notes")
+    public ResponseEntity<ProduitDto> modifierNotesProduit(@PathVariable UUID uuid,
+                                                           @Valid @RequestBody UpdateNoteProduitDto updateNoteProduitDto) throws ProduitException {
+        logger.info("Modification des notes du produit : {}", uuid);
+        ProduitDto produitDto = produitService.modifierNotesProduit(uuid,updateNoteProduitDto.notes())
+                .orElseThrow();
+        return ResponseEntity.ok().body(produitDto);
+    }
+    @PutMapping("/modifierProduit")
+    public ResponseEntity<ProduitDto> modifierProduit(@Valid @RequestBody UpdateProduitDto produitDto) throws ProduitException {
+        logger.info("Modification du produit : {}", produitDto.uuid());
+        ProduitDto produitModifie = produitService.modifierProduit(produitDto).orElseThrow();
+        return ResponseEntity.ok().body(produitModifie);
+    }
+
+    @DeleteMapping("/{uuid}/effacerProdutit")
+    public ResponseEntity<ProduitDto> supprimerProduit(@PathVariable UUID uuid) throws ProduitException {
+        logger.info("Suppression du produit : {}", uuid);
+        ProduitDto produitDto = produitService.supprimerProduit(uuid).orElseThrow();
+        return ResponseEntity.ok().body(produitDto);
+    }
+
+    @GetMapping("/famille-code")
+    public ResponseEntity<List<ProduitDto>> obtenirProduitsParFamille(@RequestParam(name = "id", required = true) UUID familleUuid) throws FamilleException {
+        logger.info("Récupération des produits de la famille : {}", familleUuid);
+        List<ProduitDto> produitDto = produitService.obtenirProduitsParFamille(familleUuid);
+        return ResponseEntity.ok().body(produitDto);
+    }
+
+    @GetMapping("/liste-alertes-achats")
+    public ResponseEntity<List<ProduitDto>> genererListeAlertesEtAchats(){
+        logger.info("Génération de la liste d'alertes et d'achats");
+        List<ProduitDto> produitDto = produitService.genererListeAlertesEtAchats();
+        return ResponseEntity.ok().body(produitDto);
+    }
 }
